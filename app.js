28f111b387506547b514f995cfbb2619f605fad2
ShaharMachluf <shahar6261@gmail.com>
2023-05-23 12:58:36 +0300

    ml

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 import express from 'express';
 import { getRouter } from "./routers/gets.js"
 import { postRouter } from "./routers/posts.js"
+import { mlRouter } from "./routers/ml.js"
 import { createRequire } from "module";
+import * as ml_functions from "./model/dataset.js"
 
 const app = express()
 app.use(postRouter)
 app.use(getRouter)
+app.use(mlRouter)
 // app.use(express.static('routers'))
 
 const require = createRequire(import.meta.url);
-const qrcode = require('qrcode-terminal');
+// const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
+// const { Client } = require('whatsapp-web.js');
 
-const client = new Client();
-async function createQr(){
-    client.on('qr', qr => {
-        qrcode.generate(qr, {small: true});
-        console.log(client.info)
-    });
+ml_functions.create_dataset()
+
+// const client = new Client();
+// async function createQr(){
+//     client.on('qr', qr => {
+//         qrcode.generate(qr, {small: true});
+//         console.log(client.info)
+//     });
  
-}
+// }
 
-createQr().then(()=>{
-    app.listen(3000,  () => {
-        console.log("Listening on port 3000...")
+// createQr().then(()=>{
+//     app.listen(3000,  () => {
+//         console.log("Listening on port 3000...")
         
-    })
-})
+//     })
+// })
 
-client.initialize();
-export { client, require }
+// client.initialize();
+// export { client, require }
 
---
+++ model/dataset.js
@@ -0,0 +1,66 @@
+import { initializeApp } from 'firebase/app';
+import * as fb from 'firebase/firestore'
+import 'firebase/auth';
+import 'firebase/firestore'
+import { where } from 'firebase/firestore';
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+var fs = require('fs');
+
+//connect to firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAMCzLhOCo9FEZTEB8sSEOtmFGjkinb74o",
+    authDomain: "1:1056818816676:android:0496c991d8b439fae82831.firebaseapp.com",
+    databaseURL: "https://JoinMe.firebaseio.com",
+    projectId: "joinme-423bf"
+    // storageBucket: "<BUCKET>.appspot.com",
+    // messagingSenderId: "<SENDER_ID>",
+  };
+
+const fireBaseRef = initializeApp(firebaseConfig);
+const db = fb.getFirestore(fireBaseRef);
+
+async function get_groups(){
+    const q = fb.query(fb.collection(db, 'groups'));
+    const groupsSnapshot = await fb.getDocs(q)
+    const groupListFromDB = groupsSnapshot.docs || []
+    const groupList = groupListFromDB.map(doc=> {
+    const {title, participants} = doc.data()
+    return {title, participants}
+  })
+  return groupList
+}
+
+export async function create_dataset(){
+    try {//if the file exist, delete it and create a new one
+        if (fs.existsSync("model.csv")) {
+            fs.unlinkSync("model.csv");
+        }
+    } catch (e) {
+        console.error(e);
+    }
+    //fields
+    var csv = "uid, category\r\n";
+    await fs.appendFile("model.csv", csv, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("success");
+        }
+    });
+    var groupList = get_groups()
+    for(const group of groupList){
+        for(const participant of group.participants){
+            csv = participant +","+group.title+"\r\n"
+
+            fs.appendFile("model.csv", csv, (err)=>{
+                if (err){
+                    console.log(err)
+                }
+            });
+        }
+    }
+    
+}
